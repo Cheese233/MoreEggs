b7a5f9733f1a3da56399b0324016c1a6f2e4c9b9
Cheese233 <13796306138@163.com>
2017-02-23 02:24:23 +0800

    add ItemDiamondEggPickaxe frame

---
+++ src/main/java/com/eggy648/moreeggs/item/ItemDiamondEggPickaxe.java
@@ -0,0 +1,22 @@
+package com.eggy648.moreeggs.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemPickaxe;
+import net.minecraftforge.common.util.EnumHelper;
+
+/**
+ * Created by Cheese on 2017/2/23.
+ * Define Item: DiamondEgg Pickaxe
+ */
+public class ItemDiamondEggPickaxe extends ItemPickaxe{
+    public static final Item.ToolMaterial DiamondEgg=EnumHelper.addToolMaterial("DiamondEgg",
+            3,
+            7000,
+            18F,
+            4.5F,
+            14
+            );
+    public ItemDiamondEggPickaxe() {
+        super(DiamondEgg);
+    }
+}
