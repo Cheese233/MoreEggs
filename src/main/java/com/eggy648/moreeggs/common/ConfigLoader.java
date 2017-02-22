6ca70cb05f713fbed04af9fd4ab1e27358cdbe4c
Cheese233 <13796306138@163.com>
2017-02-22 13:45:37 +0800

    update ConfigLoadder

--- src/main/java/com/eggy648/moreeggs/common/ConfigLoader.java
+++ src/main/java/com/eggy648/moreeggs/common/ConfigLoader.java
@@ -1,7 +1,38 @@
 package com.eggy648.moreeggs.common;
 
-/**
- * Created by Cheese on 2017/2/22.
- */
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+
 public class ConfigLoader {
+    private static Configuration config;
+    private static Logger logger;
+    private static int diamondBurnTime;
+
+    public ConfigLoader(FMLPreInitializationEvent event)
+    {
+        logger= event.getModLog();
+        config=new Configuration(event.getSuggestedConfigurationFile());
+
+        config.load();
+        load();
+
+    }
+    public static void load()
+    {
+        logger.info("Started load config");
+
+        StringBuilder comment=new StringBuilder();
+        comment.append("How many second can a diamond burn int a furnace.");
+
+        diamondBurnTime=config.get(Configuration.CATEGORY_GENERAL,"diamondBurnTime",640,comment.toString()).getInt();
+
+        config.save();
+        logger.info("Finished load config");
+    }
+    public static Logger logger()
+    {
+        return logger;
+    }
 }
